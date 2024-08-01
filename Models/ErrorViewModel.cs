60d85b123ccdd04efbbb32e44a39223b895c2420
Таня <@DESKTOP-FAGQ58D>
2024-08-01 16:02:33 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GreenShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
